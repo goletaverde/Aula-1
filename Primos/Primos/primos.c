e22354d50de0979f65ce248f9fafbd77f681b30f
Henrique Liviero <123580905+goletaverde@users.noreply.github.com>
2023-01-25 16:50:00 -0300

    Add files via upload

---
+++ Primos/Primos/primos.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int main(void) {
+	int i,teste,primomenos;
+	
+	printf("Insira o valor do nûmero que é para ser testado");
+	scanf_s("&i", i);
+	i = teste;
+	primomenos = teste - 1;
+	printf("&i",primomenos);
+	return 0;
+
+
+
+}
