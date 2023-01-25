c842f0dafa9f3203ba69846708d49147f36b9c76
Henrique Liviero <123580905+goletaverde@users.noreply.github.com>
2023-01-25 17:16:02 -0300

    Add files via upload

--- Primos/Primos/primos.c
+++ Primos/Primos/primos.c
@@ -1,15 +1,25 @@
 #include "stdio.h"
+#include "locale.h"
+#define _CRT_SECURE_NO_WARNINGS
 
 int main(void) {
-	int i,teste,primomenos;
+	setlocale(LC_ALL, "");
+
+	int i,numero,resultado = 0;
 	
 	printf("Insira o valor do nûmero que é para ser testado");
-	scanf_s("&i", i);
-	i = teste;
-	primomenos = teste - 1;
-	printf("&i",primomenos);
-	return 0;
+	scanf_s("%i", &numero);
 
-
-
+	for (i = 2; i < numero; i++){
+		if (numero % i == 0){
+			resultado++;
+			break;
+		}
+	}
+	if (resultado == 0){
+		printf("o nûmero é primo");
+	}
+	else {
+		printf("o nûmero não é primo");
+	}
 }
